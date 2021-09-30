9652e312c27cf83f402be660906e23a85b397405
7 Berna <bernardessep@gmail.com>
2021-09-30 11:44:40 -0300

    Classes AlunoDePos, ProfessorHorista e ProfessorPesquisador adicionadas

---
+++ AlunoDePos.java
@@ -0,0 +1,7 @@
+public class AlunoDePos {
+    private String nome;
+    private int idade;
+    private char conceito;
+
+    //getters e setters
+}
---
+++ ProfessorPesquisador.java
@@ -0,0 +1,8 @@
+public class ProfessorPesquisador {
+    private String nome;
+    private int idade;
+    
+    private void pesquisar(){
+        System.out.printf("%s esta pesquisando sobre...\n", nome);
+    } 
+}
